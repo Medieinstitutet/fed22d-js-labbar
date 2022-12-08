51f168609c7759aa225bb07ac7824719b1011ddf
Jenni <jenni@ohsomodern.org>
2022-12-08 17:44:21 +0200

    (feat): limit movement

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import './style.scss';
 import gsap from 'gsap';
 
+const board: HTMLDivElement | null = document.querySelector('#gameboard');
 const player: HTMLDivElement | null = document.querySelector('#player');
+let boardWidth = 0;
+let playerWidth = 0;
 let posX = 0;
 
 // ------------------------------------------------------------------------------------
@@ -11,11 +14,17 @@ let posX = 0;
 function movePlayer(e: KeyboardEvent): void {
   if (e.key === 'ArrowRight') {
     posX += 10;
+    if (posX >= boardWidth - playerWidth) {
+      posX = boardWidth - playerWidth;
+    }
     gsap.to(player, { x: posX });
   }
 
   if (e.key === 'ArrowLeft') {
     posX -= 10;
+    if (posX <= 0) {
+      posX = 0;
+    }
     gsap.to(player, { x: posX });
   }
 
@@ -24,6 +33,8 @@ function movePlayer(e: KeyboardEvent): void {
 // ------------------------------------------------------------------------------------
 // ------------------------------------------ INIT ------------------------------------
 // ------------------------------------------------------------------------------------
-if (player !== null) {
+if (player !== null && board !== null) {
   window.addEventListener('keydown', movePlayer);
+  boardWidth = board?.getBoundingClientRect().width;
+  playerWidth = player.getBoundingClientRect().width;
 }
