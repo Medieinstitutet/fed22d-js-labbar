b33fdd06cebecdd6b5d9bf8f27b26d9cdf84cba5
Jenni <jenni@ohsomodern.org>
2023-01-10 16:21:10 +0200

    Actually use the switch :joy:

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import HeaderNav from './components/HeaderNav.vue';
 import HeroImage from './components/HeroImage.vue';
+import ThemeSwitch from './components/ThemeSwitch.vue';
 </script>
 
 <template>
   <div>
     <HeaderNav class="test" />
     <HeroImage />
+    <ThemeSwitch />
     <div>
       page content
     </div>
