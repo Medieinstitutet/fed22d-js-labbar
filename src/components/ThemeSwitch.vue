b88ac73bed4cfb87af4f1252ce65207a4aa1c38b
Jenni <jenni@ohsomodern.org>
2023-01-10 16:01:53 +0200

    resultat themeswitch

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,48 @@
+<script>
+import { avokado, } from './avokado';
+
+    export default {
+        data() {
+            return {
+                button: false,
+                avokado: avokado,
+                symbol: ['avokado', 'banan'],
+            }
+        },
+
+        methods: {
+            handleBtnClick() {
+                this.button = !this.button;
+                document.body.classList.toggle("dark");
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <button @click="handleBtnClick" :class="{ 'button': button }">
+    {{     avokado.color }}
+    {{   avokado.ripeness  }}
+        <span v-if="button">{{ symbol[1] }}</span>
+       <span v-if="!button">{{ symbol[0] }}</span>
+    </button>
+</template>
+
+
+
+<style lang="scss">
+  .dark {
+    background-color: rgb(39, 38, 38);
+  }
+
+  .button {
+    background-color: yellow;
+  }
+
+  button {
+    background-color: green;
+  }
+
+</style>
+
---
+++ src/components/avokado.js
@@ -0,0 +1,11 @@
+export const avokado = {
+  weight: 100,
+  ripeness: 'moldy',
+  color: 'green',
+};
+
+export const banan = {
+  weight: 200,
+  ripeness: 'ripe',
+  color: 'yellow',
+};
