8819486579cda88c1e4ce636dea73a953e1948a6
Jenni <jenni@ohsomodern.org>
2023-01-10 17:14:37 +0200

    Vue loop & SVG-animation

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import CardElement from './components/CardElement.vue';
+import LogoSvg from './components/LogoSvg.vue';
+import banana from './assets/banana.jpg';
+import strawberry from './assets/strawberry.jpg';
+
+const cardData = ref([
+  {
+    title: 'Banan',
+    desc: 'Lorem',
+    img: {
+      url: banana,
+      alt: 'Banan'
+    },
+  },
+  {
+    title: 'Avokado',
+    desc: 'Ipsum ...'
+  },
+  {
+    title: 'Jordgubbe',
+    desc: 'Dolor',
+    img: strawberry,
+    img: {
+      url: strawberry,
+      alt: 'Jordgubbe'
+    },
+  },
+  {
+    title: 'Blåbär',
+    desc: 'Sit'
+  },
+  {
+    title: 'Hallon',
+    desc: 'Amet'
+  },
+]);
+</script>
+
+<template>
+  <div>
+    <LogoSvg />
+  </div>
+  <CardElement 
+    v-for="card in cardData"
+    :key="card.title"
+    :card-title="card.title"
+    :desc="card.desc"
+    :img="card.img"
+     />
+</template>
---
+++ src/components/CardElement.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps(['cardTitle', 'desc', 'img']);
+</script>
+
+<template>
+  <article>
+    <h2>{{ cardTitle }}</h2>
+    <template v-if="img">
+      <img :src="img.url" :alt="img.alt" />
+    </template>
+    <p>{{ desc }}</p>
+  </article>
+</template>
+
+<style scoped lang="scss">
+article {
+  background-color: rgb(89, 89, 89);
+  border-radius: 8px;
+  min-width: 300px;
+  min-height: 300px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 8px;
+  h2, p {
+    width: 100%;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+</style>
